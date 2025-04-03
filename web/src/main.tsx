cb76aeab9725a223229d8e7f49b6c9a52bcf7680
Vanessa Depraute <vanessadepraute@gmail.com>
2025-04-03 03:03:02 +0200

    up

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/react";
+
 // import { Pointer } from "@/components/magicui/pointer";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    {/* <Pointer className="fill-blue-500" /> */}
+    <Analytics />
   </StrictMode>
 );
